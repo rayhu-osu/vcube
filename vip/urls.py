d14acd05a471fc03aec1b1f11e4ad46ea5d3f230
Raymond <ismitawin@gmail.com>
2017-08-29 18:38:43 -0400

    small fix

--- vip/urls.py
+++ vip/urls.py
@@ -11,6 +11,5 @@ urlpatterns = [
     url(r'^index/$', views.index, name='index'),
     url(r'^(?P<category_name>[a-zA-Z\s]*)/$', views.category, name='category'),
     url(r'^(?P<store_id>[0-9]+)/$', views.store, name='store'),
-    url(r'^(?P<item_id>[0-9]+)/$', views.detail, name='detail'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
